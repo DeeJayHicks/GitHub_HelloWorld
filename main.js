9fd7a60ad808c9c4d3574bbb70ecc88c8dfba5b5
DeeJayHicks <connor.itouch@gmail.com>
2013-02-06 09:07:41 -0500

    0.0.3

---
+++ main.js
@@ -0,0 +1,41 @@
+
+        function function1(x)
+        {
+           x.style.backgroundColor="red";
+        }
+            
+        
+        
+        function function2(x)
+        {
+            x.style.backgroundColor="blue";
+        }
+                    
+            
+       
+        function function3(x)
+        {
+            x.style.backgroundColor="green";
+        }
+         
+            
+        
+        function function4(x)
+        {
+            x.style.backgroundColor="purple";
+        }
+
+    
+        function clear()
+        {
+            location.reload();
+        }
+
+        function imageChange(id)
+        {
+           id.src="images/image2.jpg";
+        }
+        
+    
+       
+           
